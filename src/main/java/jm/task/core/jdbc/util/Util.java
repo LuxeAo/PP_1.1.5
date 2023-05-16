db24ba529aaf2df3d1bfdfc24aff80c0967b4df9
LuxeAo <121669318+LuxeAo@users.noreply.github.com>
2023-05-16 22:49:38 +0400

    Все тесты пройдены

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/database";
+    private static final String DB_USERNAME = "root1";
+    private static final String DB_PASSWORD = "root1";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+            connection.setAutoCommit(false);
+            System.out.println("Connection OK");
+        } catch (ClassNotFoundException  | SQLException e) {
+            System.out.println("Error connection");
+        }
+        return connection;
+    }
 }
